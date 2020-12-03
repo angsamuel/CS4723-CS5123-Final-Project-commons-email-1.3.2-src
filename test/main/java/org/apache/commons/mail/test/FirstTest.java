ad48767c8b24dea2b42d716eb9639bbac97283b1
Samuel Ang <samuel.ang.prog@gmail.com>
2020-12-03 01:17:40 -0600

    Added some tester code to the test classes

--- test/main/java/org/apache/commons/mail/test/FirstTest.java
+++ test/main/java/org/apache/commons/mail/test/FirstTest.java
@@ -10,11 +10,32 @@ public class FirstTest extends TestCase{
 	Email testEmail;
 	public void setUp() {
 		testEmail = new SimpleEmail();
+		System.out.println("");
+		System.out.println("User ID: ccr___");
+		System.out.println("This test covers the AddBcc, AddBccMulti methods of Email.");
+		System.out.println("");
+		
 	}
+	
+	//Test Add single BCC
 	public void testAddBcc() throws EmailException {
-		System.out.println("This is the first test!");
 		testEmail.addBcc("a@b.com");
-		assertEquals("a@b.com", testEmail.getBccAddresses().get(0).toString());
+		assertEquals("First Bcc address was expected to be a@b.com but was instead " + testEmail.getBccAddresses().get(0).toString(), "a@b.com", testEmail.getBccAddresses().get(0).toString());
 	}
+	
+	//TEST ADD BCC Multiple
+	public void testAddBccMulti() throws EmailException {
+		String[] addresses = {"a@b.com", "b@c.com","c@d.com","d@e.com"};
+		testEmail.addBcc(addresses);
+		
+		//run through each address and ensure they match the array we passed
+		for(int i = 0; i<addresses.length; i++) {
+			assertEquals("added Bcc at position " + i + " was supposed to be " + addresses[i] + " but instead was " + testEmail.getBccAddresses().get(i).toString(),
+					addresses[i],testEmail.getBccAddresses().get(i).toString());
+		}
+		
+	}
+	
+	
 
 }
--- test/main/java/org/apache/commons/mail/test/SecondTest.java
+++ test/main/java/org/apache/commons/mail/test/SecondTest.java
@@ -1,5 +1,7 @@
 package org.apache.commons.mail.test;
 
+import javax.mail.internet.MimeMultipart;
+
 import org.apache.commons.mail.Email;
 import org.apache.commons.mail.EmailException;
 import org.apache.commons.mail.SimpleEmail;
@@ -12,9 +14,70 @@ public class SecondTest extends TestCase{
 		testEmail = new SimpleEmail();
 	}
 	public void testAddBcc() throws EmailException {
-		System.out.println("This is the second test!");
-		testEmail.addBcc("a@b.com");
-		assertEquals("a@b.com", testEmail.getBccAddresses().get(0).toString());
+		System.out.println("");
+		System.out.println("User ID: ccr___");
+		System.out.println("This test covers the buildMimeMessage method of Email under a variety of conditions (without subject, without from addr, without reciever, with full info.");
+		System.out.println("");
+		
 	}
+		
+	public void testBuildMimeMessageWithoutHost() throws EmailException {
+		try {
+			testEmail.buildMimeMessage(); fail("Mime message cannot be built without host.");
+		}catch(EmailException e) {
+			assertEquals("Cannot find valid hostname for mail session",e.getMessage());
+		}
+	}
+	
+		//this method needs to be tested without subject, from, to, message
+	
+	public void testBuildMimeMessageWithoutSubject() throws EmailException {
+		testEmail.setHostName("email.com");
+		try {
+			testEmail.buildMimeMessage(); fail("Mime message cannot be built without subject.");
+		}catch(EmailException e) {
+			;
+		}
+	}
+	
+	public void testBuildMimeMessageWithoutFrom() throws EmailException {
+		testEmail.setHostName("email.com");
+		testEmail.setSubject("test subject");
+		try {
+			testEmail.buildMimeMessage(); fail("Mime message cannot be built without from address.");
+		}catch(EmailException e) {
+			;
+		}
+	}
+	
+	public void testBuildMimeMessageWithoutReciever() throws EmailException {
+		testEmail.setHostName("email.com");
+		testEmail.setSubject("test subject");
+		testEmail.setFrom("from@email.com");
+		try {
+			testEmail.buildMimeMessage(); fail("Mime message cannot be built without reciever address.");
+		}catch(EmailException e) {
+			;
+		}
+	}
+	
+	public void testBuildMimeMessageFull() throws EmailException {
+		//add minimum requirements for valid call
+		testEmail.setHostName("email.com");
+		testEmail.setSubject("test subject");
+		testEmail.setFrom("from@email.com");
+		testEmail.addTo("reciever@gmail.com");
+		
+		//add optional content to increase test coverage
+		testEmail.addCc("cc@email.com");
+		testEmail.addBcc("bcc@email.com");
+		testEmail.updateContentType("text/plain; charset=UTF-8");
+		testEmail.setContent(new MimeMultipart());
+		testEmail.addHeader("header name", "header value");
+		
+		//finally call the actual method
+		testEmail.buildMimeMessage();
+	}
+	
 
 }
